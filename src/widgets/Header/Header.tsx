6a408293487f3231f070555cd36f2a51e7640112
Александр <aleksandr@Noutbuk-Aleksandr.local>
2026-02-28 15:46:31 +1000

    Добавил ссылки

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router";
+
 export default function Footer() {
-    return <>Header</>
+    return <>
+        <h1>Header</h1>
+        <Link to={"/"}>Главная</Link>
+        <Link to="/catalog">Каталог</Link>
+        <Link to="/product/:id">Продукт</Link>
+        <Link to="/cart">Карт</Link>
+    </>
 }
